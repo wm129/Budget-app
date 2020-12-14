db5e30593acebac6125ea8c05626e05440996ec3
wm129 <sekaimanyuucamp@gmail.com>
2020-12-14 18:00:04 +0900

    useContext

--- src/context/AuthService.jsx
+++ src/context/AuthService.jsx
@@ -1,11 +1,22 @@
-import React, {createContext} from 'react';
+import React, {createContext, useState, useEffect } from 'react';
+import { auth } from '../config/firebase';
+import firebase from './config/firebase';
 
 const AuthContext = createContext()
 
 const AuthProvider = ({children}) => {
-const someValue = "hi"
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        auth().onAuthStateChanged(user =>{
+            setUser(user)
+        })
+    }, [])
+
+
+
 return (
-<AuthContext.Provider value={someValue}>{children}</AuthContext.Provider>
-    );
+<AuthContext.Provider value={user}>{children}</AuthContext.Provider>
+    )
 };
 export { AuthContext, AuthProvider };
