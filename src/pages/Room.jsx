69c0d4114a4ec6fae1534559b7efaac1ec0edc68
wm129 <sekaimanyuucamp@gmail.com>
2020-12-16 18:55:47 +0900

    add TextFieled and button

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -2,9 +2,23 @@ import React,{useContext, useEffect, useState} from 'react';
 import {auth, db} from "../config/firebase"
 import {AuthContext} from '../context/AuthService';
 
+
+import {makeStyles} from "@material-ui/core/styles";
+import {Button, Card, TextField, Typography} from "@material-ui/core";
+
+const useStyles = makeStyles({
+budget: {
+    padding: "15px",
+    margin: "15px",
+},
+})
+
+
 const Room = () => {
-const [budget, setBudget] = useState([]);    
+const [budget, setBudget] = useState([]); 
+const [figure, setFigure] = useState('');   
 const user = useContext(AuthContext);
+const classes = useStyles();
 
 const signout = () => {
     auth.signOut();
@@ -23,17 +37,25 @@ useEffect(() => {
 return (
     <>
         <h1>Room</h1>
-        <ul>
             {budget.map((budget) => {
                 return (
-                <li key={budget.id}>
-                    <p>user: {budget.name}</p>
-                    <p>{budget.expensive}</p>                    
-                </li>
+                <Card key={budget.id} className={classes.budget}>
+                    <Typography>user: {budget.name}</Typography>
+                    <Typography>{budget.expensive}</Typography>                    
+                </Card>
                 );
             })}
-        </ul>
-        <button onClick={signout}>Logout</button>
+        <form>
+            <TextField 
+            variant='filled'
+            placeholder="plese write cost in this area"
+            value={figure} 
+            onChange={(e) => setFigure(e.target.value)} 
+            />
+            <Button type="submit" variant="contained" color="secondary">Send</Button>
+        </form>
+
+        <Button onClick={signout} variant='contained'>Logout</Button>
     </>
     );
 };
