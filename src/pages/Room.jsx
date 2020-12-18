4f8b6a824e0e3d7c0cb8739902befd15589e13eb
wm129 <sekaimanyuucamp@gmail.com>
2020-12-18 17:27:25 +0900

    Failed to compile #5-2

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import {auth} from "../config/firebase"
+import React, { useState } from 'react';
+import {auth, db} from "../config/firebase"
 
 import List from '../components/List';
 import Form from '../components/Form';
-
+/* import Check from '../components/Check'; */
 import {Button} from "@material-ui/core";
 
 
@@ -12,11 +12,24 @@ const Room = () => {
 const signout = () => {
     auth.signOut();
 };
+
+const [budget, setBudget] = useState([]);
+db.collection('budget').where('expensive')
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            console.log(doc.id, "=>", doc.data());
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting document", error);
+    });
 return (
     <>
         <h1>Room</h1>
             <List />
             <Form />
+
         <Button onClick={signout} variant='contained'>Logout</Button>
     </>
     );
