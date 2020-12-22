5e29214c75e5f7bef249c0daae3a84f18015f177
wm129 <sekaimanyuucamp@gmail.com>
2020-12-22 20:52:08 +0900

    get Time

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -14,9 +14,10 @@ const Item = ({budget}) => {
     const classes = useStyles();
     //const date = budget.createdAt;
     //const dateStamp = date.toDate();
+    console.log(budget.createdAt.toDate); //JSX objectをそのまま返していた　＝＞　toString()で文字に型を変更して渡すと解決
     return(
         <Card key={budget.id} className={classes.budget}>
-                <Typography>wtitten by: {budget.name}, date: {budget.createdAt.toDate} </Typography>
+                <Typography>wtitten by: {budget.name}, date: {budget.createdAt.toDate().toString()} </Typography>
                 <Typography>expense: {budget.expensive}yen</Typography>                    
         </Card>
     );
